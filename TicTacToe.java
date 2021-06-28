5f795f682bfc00744b9d65251172f411793573fc
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-27 17:48:21 -0700

    board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,23 @@
-class TicTacToe
+class Board
 {
-  public static void main(String[] args)
-  {
-    System.out.println("welcome to tictac toe game");
+   public static void main(String[] args)
+   {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        Board();
+   }
+
+   public static void Board()
+   {
+    char[] board=new char[8];
+    for(int i=0;i<=2;i++)
+    {
+        for(int j=0;j<=2;j++)
+        {
+            System.out.print('_');
+            System.out.print('|');
+        }
+        System.out.println();
+    }
    }
 }
 
