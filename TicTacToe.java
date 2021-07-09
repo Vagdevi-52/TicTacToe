41a899a335fad36176e64c4f0307f6eea18e5396
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-08 21:57:08 -0700

    Added: Toss To Check Who Plays First

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
+import java.util.Random;
 
-class TicTacToe
+public class TicTacToe
  {
 	static char[] board = new char[10];
 	static char playerOption,computerOption;
+	static int[] computerNum;
 	public static Scanner scanner = new Scanner(System.in);
+	static final Random random = new Random();
 
 	public static void main(String[] args)
-        {
+       {
 		getBoard();
 		getPlayerChoice();
 		currentBoard();
-                userMove();
-                desiredMove();
+		whoPlayFirst();
+		userMove();
+		desiredMove();
 	}
 
-	 static void getBoard() //method for board
+	 static void getBoard()//Method For Board 
          {
 		 for (int i = 0; i < 10; i++) 
                  {
@@ -23,7 +27,7 @@ class TicTacToe
 		 }
 	 }
 
-	 static void getPlayerChoice() //Method to choose the option
+	 static void getPlayerChoice()//method to choose the option 
          {
 		 System.out.print("Choose an Option x or o : ");
 		 playerOption = scanner.next().charAt(0);
@@ -33,7 +37,7 @@ class TicTacToe
 			 computerOption ='x';
 		 System.out.println("You Selected : " +playerOption);
 	 }
-	 static void showBoard()//Method for print the board
+	 static void showBoard() //method to print board
          {
 	        System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
 	        System.out.println(".....|......|.....");
@@ -41,7 +45,7 @@ class TicTacToe
 	        System.out.println(".....|......|.....");
 	        System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
 	 }
-	 private static void currentBoard()
+	 private static void currentBoard() 
          {
 	        int RADIX = 10;
 	        System.out.println("\n");
@@ -50,29 +54,24 @@ class TicTacToe
 	            if (board[i] !='x'&&board[i] !='o')
 	                board[i] = Character.forDigit(i, RADIX);
 	        }
-
-	        showBoard(); //call showboard method
-	  }
-        static void userMove() //userMove
-         {
-	        System.out.print("\nSelect the cell from 1 to 9 : ");
-	        int userChoice = scanner.nextInt();
-	        if (board[userChoice] != 'x' && board[userChoice] != 'o')
-                {
-	            board[userChoice] = playerOption;
-	        }
-              else
-              {
-	            System.out.println("Invalid Cell");
-	        }
-	        //calling current board method
-	        currentBoard();
-	    }
-       static void desiredMove() //method for usermove to check free space
-       {
+	        showBoard();//show board
+	 }
+	 static void userMove() 
+        {
 	        System.out.print("\nSelect the Cell from 1 to 9 : ");
 	        int userChoice = scanner.nextInt();
-	        if (board[userChoice] != 'x' || board[userChoice] != 'o') 
+	        if (board[userChoice] != 'x' && board[userChoice] != 'o') {
+	            board[userChoice] = playerOption;
+	        } else {
+	            System.out.println("Invalid Cell");
+	        }
+	        currentBoard();//caling current board
+	 }
+	    static void desiredMove() //method for user move to check free space
+            {
+	        System.out.print("\nSelect the Cell from 1 to 9 : ");
+	        int userChoice = scanner.nextInt();
+	        if (board[userChoice] != 'x' || board[userChoice] != 'o')
                 {
 	            board[userChoice] = playerOption;
 	        }
@@ -80,9 +79,47 @@ class TicTacToe
                 {
 	            System.out.println("Invalid Cell");
 	        }
-	        currentBoard(); //calling current board 
+	        currentBoard();//calling current board
 	       userMove();
-	 }
+	   }
+	    static void whoPlayFirst() 
+            {
+	        System.out.println("\nMaking toss to check who play first\n\nselect the number 1 for head and 2 for tail\n ");
+	        int check = scanner.nextInt();
+	        if (check == 1 || check == 2)
+                {
+	            int flip = random.nextInt(2) + 1;
+	            if (flip == 1) 
+                   {
+	                System.out.println("HEAD");
+	            }
+                   else
+                    {
+	                System.out.println("TAIL");
+	            }
+	            if (flip == check) 
+                    {
+	                System.out.println("You Won the Toss You will Start the Game\n");
+	                desiredMove();
+	            }
+                  else
+                   {
+	                System.out.println(" You Lost computer will Start the Game \n");
+	                computerMove();
+	            }
+	        } else 
+                  {
+	            System.out.println("\nInvalid input Again\n");
+	           whoPlayFirst();
+	          }
+	    }
+	         //creating  for computer move
+	         static void computerMove() 
+                 {
+	            computerNum = new int[9];
+	            int  computerChoice = random.nextInt(9)+1;
+	            board[computerChoice]=computerOption;
+	            System.out.println("the computer choice "+computerChoice+"' now its your turn.");
+	            desiredMove();
+	        }
 }
-
-
